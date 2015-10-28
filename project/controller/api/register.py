8b9ebb8e92777306b8c09bed895ada541ea70507
Ali Fazaeli <a.fazaeli1992@gmail.com>
2015-10-29 01:08:02 +0330

    change on register and login

--- project/controller/api/register.py
+++ project/controller/api/register.py
@@ -2,23 +2,45 @@ __author__ = 'ali'
 
 # from python and Flask
 from flask import request, jsonify
+from sqlalchemy.exc import IntegrityError
 
 # from project
 from . import api
 from project.models.user import User
 
-@api.route('/add_user', methods=['get', 'POST'])
+
+@api.route('/add_user', methods=['GET', 'POST'])
 def add_user():
 
-    user = User()
-    user.user_name = request.form.get('user_name', '')
-    user.device_id = request.form.get('device_id', '')
-    user.devices = request.form.get('devices', '')
-    user.email = request.form.get('email', '')
-    user.password = request.form.get('password', '')
-    user.image = request.form.get('image', '')
-    user.add_user()
+    try:
+        user = User()
+        user.user_name = request.form.get('user_name', None)
+        user.device_id = request.form.get('device_id', '')
+        user.devices = request.form.get('devices', '')
+        user.email = request.form.get('email', None)
+        user.password = request.form.get('password', None)
+        user.image = request.form.get('image', '')
+        user.token = hash(user.email)
+        user.add_user()
 
-    return '200'
+        return jsonify(status=200, token=user.token)
+    except IntegrityError:
+        return jsonify(sataus=500, data='Duplicate_email')
+    except Exception as ex:
+        return jsonify(status=500, data=str(ex.args))
 
 
+@api.route('/login', methods=['POST', 'GET'])
+def login():
+
+    email = request.form.get('email', '')
+    password = request.form.get('password', '')
+
+    user_obj = User.get_user(email, password)
+    try:
+        if user_obj:
+            return jsonify(status=200, data=dict(user_name=user_obj.user_name, token=user_obj.token))
+        else:
+            return jsonify(status=404, data='Not Found!')
+    except Exception as ex:
+        return jsonify(status=500, data=ex.args)
--- project/models/user.py
+++ project/models/user.py
@@ -1,18 +1,32 @@
 __author__ = 'ali'
 
 from project.factory import db
+from sqlalchemy import UniqueConstraint
+from sqlalchemy.exc import IntegrityError, InvalidRequestError
 
 
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_name = db.Column(db.Unicode)
-    email = db.Column(db.Unicode)
+    email = db.Column(db.Unicode, unique=True, nullable=False)
+    user_name = db.Column(db.Unicode, nullable=False)
     password = db.Column(db.Unicode)
     devices = db.Column(db.Unicode)
     podcast_played_counter = db.Column(db.Integer)
     device_id = db.Column(db.Unicode)
+    token = db.Column(db.Unicode)
+
+    __table_args__ = (UniqueConstraint('email', name='email__unique_user_key'), )
 
     def add_user(self):
-        db.session.add(self)
-        db.session.commit()
+
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except IntegrityError as ex:
+            return
+
+    @staticmethod
+    def get_user(email, password):
+        return User.query.filter(email == email, password == password).first()
+        # return User.query.filter(**kwargs)
