b391a3c08f101973aed660233ab896f71f2a45be
Ali Fazaeli <a.fazaeli1992@gmail.com>
2015-12-03 02:17:32 +0330

    add some fields to podcast table and set relations , add get method for retrieve podcast with **kwargs

--- project/models/podcast.py
+++ project/models/podcast.py
@@ -5,8 +5,9 @@ import datetime
 
 
 class Podcast(db.Model):
+    __tablename__ = 'podcast'
 
-    id = db.Column(db.Integer, primary_key=True, autoincreament=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.Unicode)
     likes = db.Column(db.Unicode)
     links = db.Column(db.Unicode)
@@ -14,6 +15,10 @@ class Podcast(db.Model):
     note = db.Column(db.Unicode)
     url = db.Column(db.Unicode)
     image = db.Column(db.Unicode)
-    upload_time = db.Column(db.DATETIME, default=datetime.datetime.now)
-    channel = db.Column(db.Integer, db.ForeignKey('channel.id'))
-    user = db.Column(db.Integer, db.ForeignKey('User.id'))
+    upload_time = db.Column(db.DateTime, default=datetime.datetime.now)
+    channel_rel = db.Column(db.Integer, db.ForeignKey('channel.id'))
+    user_rel = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    @staticmethod
+    def get(**kwargs):
+        return Podcast.query.filter_by(**kwargs)
