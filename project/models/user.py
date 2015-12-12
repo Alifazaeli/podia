264f2625dd07e14f7f6284cac8b92e0c38a8909d
Ali Fazaeli <a.fazaeli1992@gmail.com>
2015-12-12 04:42:59 +0330

    change get user method name get_user()  to get() + convert user object to dict with as_dict()

--- project/models/user.py
+++ project/models/user.py
@@ -1,9 +1,8 @@
 s__author__ = 'ali'
 
 from project.factory import db
-from sqlalchemy import UniqueConstraint
-from flask.ext.login import UserMixin
-from sqlalchemy.exc import IntegrityError, InvalidRequestError
+from project.utils.serializer import dump_datetime
+import datetime
 
 
 class User(db.Model):
@@ -14,16 +13,36 @@ class User(db.Model):
     user_name = db.Column(db.Unicode, nullable=False)
     password = db.Column(db.Unicode)
     devices = db.Column(db.Unicode)
-    podcast_played_counter = db.Column(db.Integer)
-    device_id = db.Column(db.Unicode)
+    podcast_played_counter = db.Column(db.Integer, default=0)
+    # device_id = db.Column(db.Unicode)
     token = db.Column(db.Unicode)
 
-    # __table_args__ = (UniqueConstraint('email', name='email__unique_user_key'), )
+    def __str__(self):
+        return self.user_name + ' - ' + self.email
+
+    def as_dict(self, ignorefields):
+        """
+        this method convert sqlalchemy object to dict
+
+        :param: self : object of models
+        :type : object
+        :param : ignorefields
+        :type : list of ignore fields
+        :rtype : dict
+        """
+        result = {}
+        for key in self.__mapper__.c.keys():
+            if key not in ignorefields:
+                if type(getattr(self, key)) == datetime:
+                    result[key] = dump_datetime(getattr(self, key))
+                else:
+                    result[key] = getattr(self, key)
+        return result
 
     def add_user(self):
         db.session.add(self)
         db.session.commit()
 
     @staticmethod
-    def get_user(**kwargs):
+    def get(**kwargs):
         return User.query.filter_by(**kwargs).first()
