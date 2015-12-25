5a50b880d234c2b6032afb8c602d7dcb75ba09e4
Ali Fazaeli <a.fazaeli1992@gmail.com>
2015-12-26 03:26:10 +0330

    add podcast_like() to store liked podcasts by clients

--- project/controller/api/podcast.py
+++ project/controller/api/podcast.py
@@ -1,11 +1,14 @@
 __author__ = 'ali'
 
-from flask import request
-import json
 
 from project.models.podcast import Podcast
 from project.models.channel import Channel
+from project.models.like import PodcastLike
+from flask.ext.login import login_required, current_user
+from sqlalchemy.exc import IntegrityError
+from flask import request, jsonify
 from . import api
+import json
 
 
 @api.route('/podcast/timeline', methods=['GET'])
@@ -31,8 +34,24 @@ def podcast():
     elif channel_uuid:
         channel = Channel.get(uuid=channel_uuid)
         podcasts = Podcast.get(limit=10, channel=channel)
+
     total = []
     for item in podcasts:
         total.append(item.as_dict(['id', 'user_id', 'channel_id']))
 
     return json.dumps({'podcasts': total}), 200
+
+
+@api.route('/podcast/like', methods=['GET', 'POST'])
+@login_required
+def podcast_like():
+
+    liked_podcast = PodcastLike()
+    podcast_uuid = request.form.get('podcast_uuid', None)
+    liked_podcast.podcast = Podcast.get(1, uuid=podcast_uuid)[0]
+    liked_podcast.user = current_user
+    try:
+        liked_podcast.add()
+        return jsonify(podcast=podcast_uuid, user=current_user.uuid), 201
+    except IntegrityError:
+        return jsonify(data='duplicate_error'), 409
