d9c71b4c7fb0d02e7b1e94a9e75536879fd2e0b3
Ali Fazaeli <a.fazaeli1992@gmail.com>
2015-12-26 03:23:23 +0330

    add UniqueConstraint to PodcastLike table for  ignore duplicate user_id and podcast_id also user add() for insert liked podcasts to database

--- project/models/like.py
+++ project/models/like.py
@@ -7,8 +7,15 @@ class PodcastLike(db.Model):
     __tablename__ = 'podcastlike'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    podcast_id = db.Column(db.Integer, db.ForeignKey('podcast.id'), nullable=True)
+    podcast_id = db.Column(db.Integer, db.ForeignKey('podcast.id'), nullable=False)
     podcast = db.relation('Podcast', backref=db.backref('podcast_ref', lazy='dynamic'))
 
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     user = db.relation('User')
+
+
+    __table_args__ = (db.UniqueConstraint('podcast_id', 'user_id', name='_podcast_user_unique_key'),)
+
+    def add(self):
+        db.session.add(self)
+        db.session.commit()
